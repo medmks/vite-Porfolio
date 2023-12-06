c803a23b364c2bd6812607fa26406190c306c224
Mohamed <looklike882@gmail.com>
2023-12-06 15:03:09 +0100

    add vercel

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -11,7 +11,9 @@ const Ball= ({imageUrl}:{imageUrl:string}) => {
       <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-    <mesh castShadow receiveShadow scale={2.75}>  
+    <mesh 
+    castShadow 
+    receiveShadow scale={2.75}>  
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
         <Decal
